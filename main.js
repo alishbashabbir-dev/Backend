7861ca2b8f2d702d26b72467977becfee29650a2
Alishba <alishbashabbir7677@gmail.com>
2024-10-18 18:04:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+
+require ('dotenv').config()
+const express = require('express')  //cretaing n object from express library 
+const app = express()  ///variable of express 
+var  port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('hitestdotcom')
+})
+
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login at channel</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>Welcome to youtube</h2>')
+})
+
+app.get('/home',(req,res)=>{
+    res.send('<h1>Welcome to Home</h1>')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(` app listening on port ${port}`)
+})
